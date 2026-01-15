812e20a077ae4a4fb8be71bb32399137436ff94f
ianch <ianchuquintana2020@hotmail.com>
2026-01-15 11:41:49 -0300

    subida al repo

---
+++ script.js
@@ -0,0 +1,6 @@
+const hoy = new Date();
+
+const opciones = {year: "numeric"};
+const fechaFormateada = hoy.toLocaleDateString("es-ES", opciones);
+
+document.getElementById("fecha").textContent = fechaFormateada;
